a7b97e5e8e1c952b95572065f449c40dd2a5aaeb
Aarit Pandey <aaritpandey10@gmail.com>
2026-01-13 15:43:28 +0530

    another minor update

--- install.sh
+++ install.sh
@@ -97,7 +97,7 @@ cat >"$DESKTOP_FILE" <<EOF
 Type=Application
 Name=L.A.P.H.
 Comment=Local Autonomous Programming Helper
-Exec="$APP_DIR/main.py"
+Exec="/home/aarit/.local/bin/LAPH/venv/bin/python /home/aarit/.local/bin/LAPH/main.py"
 Icon="$APP_DIR/laph.png"
 Terminal=true
 Categories=Development;Utility;
